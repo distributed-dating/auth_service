704dbf11bf65b91c02fd018225883f198fbf03c0
ShapovalovIlia <ilia-shapvalov04@mail.ru>
2026-01-11 21:18:32 +0300

    WIP

--- src/auth_service/domain/user.py
+++ src/auth_service/domain/user.py
@@ -1,7 +1,5 @@
 from uuid import UUID, uuid4
 
-from pwdlib import PasswordHash
-
 from auth_service.domain.value_objects import UserLogin, UserPassword
 
 
@@ -20,8 +18,8 @@ class User:
     def create(cls, login: UserLogin, hashed_password: UserPassword) -> "User":
         return User(
             id=uuid4(),
-            login=login.value,
-            hashed_password=hashed_password.value,
+            login=login,
+            hashed_password=hashed_password,
         )
 
     @property
@@ -30,24 +28,8 @@ class User:
 
     @property
     def login(self) -> str:
-        return self._login.value
+        return self._login
 
     @property
     def hashed_password(self) -> str:
-        return self._hashed_password.value
-
-    def verify_password(
-        self, password: UserPassword, password_hash: PasswordHash
-    ) -> bool:
-        return password_hash.verify(password.value, self.hashed_password)
-
-    def change_password(
-        self, new_password: UserPassword, password_hash: PasswordHash
-    ) -> None:
-        self._hashed_password = password_hash.hash(new_password.value)
-
-    def get_jwt_claims(self) -> dict[str, str]:
-        return {
-            "sub": str(self._id),
-            "login": self._login,
-        }
+        return self._hashed_password
